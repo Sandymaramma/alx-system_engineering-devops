332b697816b2a4f8de5bcb3768a843447451fc26
sandymaramma <edesandraoge@gmail.com>
2023-04-21 14:19:35 -0700

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+Puppet was used to create a file in /tmp
+
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
