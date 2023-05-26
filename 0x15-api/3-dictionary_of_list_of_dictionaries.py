9386c752d13e3ad2d5b969eba07956b83652b562
sandymaramma <edesandraoge@gmail.com>
2023-05-25 20:49:19 -0700

    Export to JSON

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Exports to-do list data of a given employee to JSON format"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("{}users".format(url)).json()
+    tasks = requests.get("{}todos".format(url)).json()
+    data_dict = {
+                user.get("id"): [{"username": user.get("username"),
+                                  "task": task.get("title"),
+                                  "completed": task.get("completed")}
+                                 for task in tasks
+                                 if task.get("userId") == user.get("id")]
+                for user in users
+           }
+    # open a new JSON file in write mode
+    with open("todo_all_employees.json", "w", newline="") as file:
+        json.dump(data_dict, file)
