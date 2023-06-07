1722db814558c7b5f2120a3679b4c3e13e0bb255
sandymaramma <edesandraoge@gmail.com>
2023-06-07 01:06:17 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A function that queries the Reddit API and returns the number of subscribers """
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
