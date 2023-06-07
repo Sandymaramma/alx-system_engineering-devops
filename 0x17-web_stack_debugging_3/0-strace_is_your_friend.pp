bd733d6b25df2b9372bca0d638855b200f1a6d64
sandymaramma <edesandraoge@gmail.com>
2023-06-07 14:53:24 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,11 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+# A puppet manuscript to replace a line in a file on a server
 
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
+
